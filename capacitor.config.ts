ff1b752e19bed9e590518ebe9fafca12c35972f2
Dario Alessio Menin <darioalessio.menin@gmail.com>
2022-01-12 21:31:23 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'pwa.update.test',
+  appName: 'PWA Update Test',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
